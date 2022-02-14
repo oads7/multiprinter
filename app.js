8d560e4304d7406aa92061134cac518f01a60cea
Alejandro <oads7@outlook.com>
2022-02-13 22:20:28 -0500

    First commit

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
 console.log("Hola Multi");
+
+app.get("/", (req, res) => 
+{
+    res.send("Multiprinter pues!");
+});
+
+
+// Starting server listener
+app.listen(port, () => 
+{
+    console.log(`Example app listening on port ${port}`)
+});
