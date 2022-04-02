e92cb5fa1a90682ca929a6544bfb2bc7f7a68475
Alejandro <oscar.diaz@raddarstudios.com>
2022-04-02 09:07:09 -0500

    Update of multiprinter API

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ function entryPoint(port)
     App.use(Express.json());
 
     // Configurar cabeceras y cors
-app.use((req, res, next) => {
+App.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
