d4f6e01d9a47b2f47bbdb2fef5ea57d75421abc3
unknown <oads7@outlook.com>
2022-05-14 08:37:12 -0500

    Hard Update

--- api/controllers/nodes.js
+++ api/controllers/nodes.js
@@ -14,11 +14,25 @@ module.exports = nodesController;
 
 // Declarations and statements
 function httpGet(request, response)
+// Example route   ?node=MWW03QQMMFAO9NWE
 {
-    let success = StatusResponse.success(dbContext.getAllNodes());
+    let localID = request.query.node;
+    let index = dbContext.index(localID);
+
+    if (index == -1) 
+    {
+        let success = StatusResponse.success(dbContext.getAllNodes());
     
-    response.setHeader('Content-Type', 'application/json');
-    response.status(200).send(success);
+        response.setHeader('Content-Type', 'application/json');
+        response.status(200).send(success);
+    }
+    else
+    {
+        let success = StatusResponse.success(JSON.stringify(dbContext.getNodeByIndex(index)));
+
+        response.setHeader('Content-Type', 'application/json');
+        response.status(200).send(JSON.stringify(success));
+    }
 }
 
 function httpPost(request, response)
